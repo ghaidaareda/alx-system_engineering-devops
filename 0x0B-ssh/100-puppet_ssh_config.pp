e1f64bab670569dd0dee2fa19a4a739268f61e65
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-28 02:09:28 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,12 @@
 #using Puppet to make changes to our configuration file
-file_line{'~/.ssh/config ':
-	ensure  => present,
-	content => "# SSH client configuration\n\nHost *\n  PasswordAuthentication no\n",
+
+file_line{'Turn off password auth':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
+}
+
+file_line{'use the private key':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school'
+
 }
