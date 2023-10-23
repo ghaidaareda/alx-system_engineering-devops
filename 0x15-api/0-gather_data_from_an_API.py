c901c28ee364da43419035a935fab3383fef079c
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-24 01:02:50 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,28 +6,38 @@ for a given employee ID,
 returns information about his/her TODO list progress.
 """
 import requests
+from sys import argv
 
-def get_employee_name(id:int):
+
+def get_employee_name(id):
     url = f'https://jsonplaceholder.typicode.com/users/{id}'
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         name = data['name']
         return name
-def to_do_done(id:int):
-    if __name__ == "__main__":
-        url = f'https://jsonplaceholder.typicode.com/todos'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            total = len(data)
-            done = 0
-            completed_tasks = []
-            for item in data:
-                if item['userId'] == id and item['completed']:
-                    done += 1
-                    completed_tasks.append(item['title'])
-            employee_name = get_employee_name(id)
-            print (f'Employee {employee_name} is done with tasks {done}/{total}')
-            for task in completed_tasks:
-                print(f'\t {task}')
+
+
+def to_do_done(id):
+    url = f'https://jsonplaceholder.typicode.com/todos'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        total = len(data)
+        done = 0
+        completed_tasks = []
+        for item in data:
+            if item['userId'] == id and item['completed']:
+                done += 1
+                completed_tasks.append(item['title'])
+        employee_name = get_employee_name(id)
+        print(f'Employee {employee_name} is done with tasks {done}/{total}')
+        for task in completed_tasks:
+            print(f'\t {task}')
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        id = int(sys.argv[1])
+        to_do_done(id)
