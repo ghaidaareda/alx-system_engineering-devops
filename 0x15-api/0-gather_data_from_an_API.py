8af06f439ce8de4fa6507ba84682687cc54fb51e
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-24 00:48:56 +0300

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Python script that,
+using  REST API,
+for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+
+def get_employee_name(id:int):
+    url = f'https://jsonplaceholder.typicode.com/users/{id}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        name = data['name']
+        return name
+def to_do_done(id:int):
+    if __name__ == "__main__":
+        url = f'https://jsonplaceholder.typicode.com/todos'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            total = len(data)
+            done = 0
+            completed_tasks = []
+            for item in data:
+                if item['userId'] == id and item['completed']:
+                    done += 1
+                    completed_tasks.append(item['title'])
+            employee_name = get_employee_name(id)
+            print (f'Employee {employee_name} is done with tasks {done}/{total}')
+            for task in completed_tasks:
+                print(f'\t {task}')
