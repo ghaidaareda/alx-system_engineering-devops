b5faac15f1e47e7ef3a86a28c103c0d7710f9cb8
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-24 23:36:56 +0300

    1-export_to_CSV.py 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 """
-Python script that,
-using  REST API,
-for a given employee ID,
-returns information about his/her TODO list progress.
+extend Python script to export data in the CSV format
 """
-import csv
-import requests
-import sys
+if __name__ == '__main__':
+    import csv
+    import requests
+    import sys
 
-url_1 = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
-url_2 = f'https://jsonplaceholder.typicode.com/user/{sys.argv[1]}/todos'
-response_1 = requests.get(url_1)
-response_2 = requests.get(url_2)
+    url_1 = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+    url_2 = f'https://jsonplaceholder.typicode.com/user/{sys.argv[1]}/todos'
+    response_1 = requests.get(url_1)
+    response_2 = requests.get(url_2)
 
-data_1 = response_1.json()
-data_2 = response_2.json()
+    data_1 = response_1.json()
+    data_2 = response_2.json()
 
-with open(f'{sys.argv[1]}.csv', 'w') as file:
-    csv_writer = csv.writer(file, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL)
-    for item in data_2:
-        csv_writer.writerow([data_1['id'], data_1['username'],
-                             item['completed'], item['title']])
+    with open(f'{sys.argv[1]}.csv', 'w') as file:
+        csv_writer = csv.writer(file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for item in data_2:
+            csv_writer.writerow([data_1['id'], data_1['username'],
+                                item['completed'], item['title']])
