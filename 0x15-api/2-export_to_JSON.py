ba6309271d51f3552efbc8c79148aa8b897b938f
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-26 01:40:17 +0300

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+extend Python script to export data in the CSV format
+"""
+if __name__ == '__main__':
+    import json
+    import requests
+    import sys
+
+    url_1 = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+    url_2 = f'https://jsonplaceholder.typicode.com/user/{sys.argv[1]}/todos'
+    response_1 = requests.get(url_1)
+    response_2 = requests.get(url_2)
+
+    data_1 = response_1.json()
+    data_2 = response_2.json()
+    tasks = []
+    for item in data_2:
+        task = {"task": item['title'],
+                "completed": item['completed'],
+                "username": data_1['username']
+                }
+        tasks.append(task)
+    user_tasks = {data_1['id']: tasks}
+
+    with open(f'{sys.argv[1]}.json', 'w') as file:
+        json.dump(user_tasks, file)
