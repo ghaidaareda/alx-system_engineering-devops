b1cc0f15c8e1e99eceaec7df7b5de5c7c0bbb689
ghaidaareda <dodo.sa7afy@gmail.com>
2023-11-08 00:05:09 +0200

    1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API
+and prints the titles of the first 10 hot posts listed
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'My Agent 1'}
+    params = {"limit": 10, }
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        else:
+            return None
+    else:
+        return None
