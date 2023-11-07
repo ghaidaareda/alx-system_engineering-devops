776a315097b3a9646562101e7e7e4b4aa282eb72
ghaidaareda <dodo.sa7afy@gmail.com>
2023-11-08 00:10:52 +0200

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,6 +7,8 @@ import requests
 
 
 def top_ten(subreddit):
+    """function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'My Agent 1'}
     params = {"limit": 10, }
