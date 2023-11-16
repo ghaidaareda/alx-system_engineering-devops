1759eb297232ef80ad6a3c4bf9f9a0ed3bf9d204
ghaidaareda <dodo.sa7afy@gmail.com>
2023-11-16 15:07:04 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,13 @@
-# Increase hard file for holberton user
+#Enable the user holberton to login, open files with no errors
+
+# Increase hard file limit for holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin/'],
 }
 
 #Increase soft file limit for user holberton
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin/'],
 }
